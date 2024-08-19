19b9b31d6fab8784741f84c6e837f0638dc9b796
erenilhan <erenilhan@ss2i-digital.fr>
2024-08-20 00:43:20 +0300

    Add debug statement to log DigitalOcean server creation response

--- app/ServerProviders/DigitalOcean.php
+++ app/ServerProviders/DigitalOcean.php
@@ -121,6 +121,7 @@ class DigitalOcean extends AbstractProvider
                 'monitoring' => false,
                 'ssh_keys' => [$sshKeyId],
             ]);
+        dd($create->json());
         if ($create->status() != 202) {
             $msg = __('Failed to create server on DigitalOcean');
             Log::error('Failed to create server on DigitalOcean', $create->json());
